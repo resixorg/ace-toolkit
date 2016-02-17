8cbcbc91d7012546454591cfb1233b82f7bd0809
Thomas Peter Berntsen <tpb@translucent.dk>
2016-02-17 23:47:57 +0100

    Fix comparison syntax error.

--- app/helpers/format-score-field.js
+++ app/helpers/format-score-field.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export function formatScoreField(params/*, hash*/) {
-  if (params[0] == "true") {
+  if (params[0] === "true") {
     return Ember.String.htmlSafe('<span class="label label-danger">Yes</span>');
   } else {
     return Ember.String.htmlSafe('<span class="label label-success">No</span>');
